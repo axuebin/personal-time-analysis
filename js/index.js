56694b65fb797337ef96448e246fc9c4d62ca490
xb9207 <xbmail2013@163.com>
2016-04-11 16:42:48 +0800

    update

--- js/index.js
+++ js/index.js
@@ -12,12 +12,6 @@ function add(){
 	var gameTmp=parseInt(document.getElementById("game").value);
 	var sportTmp=parseInt(document.getElementById("sporting").value);
 	var otherTmp=parseInt(document.getElementById("other").value);
-	console.log(readTmp);
-	console.log(codeTmp);
-	console.log(eatTmp);
-	console.log(gameTmp);
-	console.log(sportTmp);
-	console.log(otherTmp);
 
 	if(readTmp+codeTmp+eatTmp+gameTmp+sportTmp+otherTmp!=24){
 		alert("请确认您输入的时间总和为24小时！")
@@ -42,18 +36,10 @@ function avg(arr){
     return avg.toFixed(2);;
 }
 
-function test(){
-    console.log(readTime);
-    console.log(avg(readTime));
-}
-
 function display() {
-	if(readTime==0&&codeTime==0&&eatTime==0&&gameTime==0&&sportTime==0&&sportTime==0){
-		alert("请您输入时间！")
-	}else{
 		$('#chart').css({
-        	height: $(window).height()*0.5,
-        	width: $(window).width()*0.5
+        	height: $("#layui-layer1").height(),
+        	width: $("#layui-layer1").width(),
     	});
 
     	var read=avg(readTime);
@@ -83,7 +69,7 @@ function display() {
                 	//name: '数据',
                 	name: '',
                 	type: 'pie',
-                	radius: ['30%', '60%'],
+                	radius: ['20%', '50%'],
                 	data: [
                     	{name: '看书', value: read},
 	                    {name: '撸代码', value: code},
@@ -92,24 +78,20 @@ function display() {
 	                    {name: '运动', value: sport},
 	                    {name: '其它', value: other}
 	                ],
-	                itemStyle: {
-	                    normal: {
-	                        label: {
-	                            show: true,
-	                            formatter: '{b} : {c}小时({d}%)',
-	                            textStyle: {
-	                                fontFamily: '微软雅黑',
-	                                fontWeight: 'bold',
-	                                fontSize: '12'
-	                            }
-	                        },
-	                        labelLine: {show: true}
-	                    }
-	                }
+	                label:{
+	                	normal:{
+	                		show: true,
+	                        formatter: '{b} : {c}小时',
+	                        textStyle: {
+	                            fontFamily: '微软雅黑',
+	                            fontWeight: 'bold',
+	                            fontSize: '12'
+	                        }
+	                	}
+	                },
 	            }
 	        ]
 	    };
 	    pie_chart.setOption(pie_option);
-	}
 }
 
