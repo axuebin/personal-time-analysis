8dde844e2ec298e8692880f78d129d818638963b
xb9207 <xbmail2013@163.com>
2016-04-12 14:36:45 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,28 +1,53 @@
-var readTime=new Array();
-var codeTime=new Array();
-var eatTime=new Array();
-var gameTime=new Array();
-var sportTime=new Array();
-var otherTime=new Array();
+var firstText=new Array();
+var secondText=new Array();
+var thirdText=new Array();
+var fourthText=new Array();
+var fifthText=new Array();
+var sixthText=new Array();
+var firstTime=new Array();
+var secondTime=new Array();
+var thirdTime=new Array();
+var fourthTime=new Array();
+var fifthTime=new Array();
+var sixthTime=new Array();
 
 function add(){
-	var readTmp=parseInt(document.getElementById("reading").value);
-	var codeTmp=parseInt(document.getElementById("coding").value);
-	var eatTmp=parseInt(document.getElementById("eating").value);
-	var gameTmp=parseInt(document.getElementById("game").value);
-	var sportTmp=parseInt(document.getElementById("sporting").value);
-	var otherTmp=parseInt(document.getElementById("other").value);
-
-	if(readTmp+codeTmp+eatTmp+gameTmp+sportTmp+otherTmp!=24){
-		alert("请确认您输入的时间总和为24小时！")
+	var firstTextTmp=document.getElementById("firstText").value;
+	var secondTextTmp=document.getElementById("secondText").value;
+	var thirdTextTmp=document.getElementById("thirdText").value;
+	var fourthTextTmp=document.getElementById("fourthText").value;
+	var fifthTextTmp=document.getElementById("fifthText").value;
+	var sixthTextTmp=document.getElementById("sixthText").value;
+	var firstTmp=parseInt(document.getElementById("firstTime").value);
+	var secondTmp=parseInt(document.getElementById("secondTime").value);
+	var thirdTmp=parseInt(document.getElementById("thirdTime").value);
+	var fourthTmp=parseInt(document.getElementById("fourthTime").value);
+	var fifthTmp=parseInt(document.getElementById("fifthTime").value);
+	var sixthTmp=parseInt(document.getElementById("sixthTime").value);
+	if(firstTmp+secondTmp+thirdTmp+fourthTmp+fifthTmp+sixthTmp!=24&&firstTextTmp!=0&&secondTextTmp!=0&&thirdTextTmp!=0&&fourthTextTmp!=0&&fifthTextTmp!=0&&sixthTextTmp!=0){
+		alert("请确认您输入内容并且输入的时间总和为24小时！");
+	}else if(firstText.length==0&&secondText.length==0&&thirdText.length==0&&fourthText.length==0&&fifthText.length==0&&sixthText.length==0){
+		alert("添加成功，请输入下一条记录！");
+		firstText.push(firstTextTmp);
+	    secondText.push(secondTextTmp);
+	    thirdText.push(thirdTextTmp);
+	    fourthText.push(fourthTextTmp);
+	    fifthText.push(fifthTextTmp);
+	    sixthText.push(sixthTextTmp);
+		firstTime.push(firstTmp);
+	    secondTime.push(secondTmp);
+	    thirdTime.push(thirdTmp);
+	    fourthTime.push(fourthTmp);
+	    fifthTime.push(fifthTmp);
+	    sixthTime.push(sixthTmp);
 	}else{
-		alert("添加成功，请输入下一条记录！")
-		readTime.push(readTmp);
-	    codeTime.push(codeTmp);
-	    eatTime.push(eatTmp);
-	    gameTime.push(gameTmp);
-	    sportTime.push(sportTmp);
-	    otherTime.push(otherTmp);
+		alert("添加成功，请输入下一条记录！");
+		firstTime.push(firstTmp);
+	    secondTime.push(secondTmp);
+	    thirdTime.push(thirdTmp);
+	    fourthTime.push(fourthTmp);
+	    fifthTime.push(fifthTmp);
+	    sixthTime.push(sixthTmp);
 	}
 }
 
@@ -37,63 +62,70 @@ function avg(arr){
 }
 
 function display() {
-	var pie_option=null;
+	
 	$('#chart').css({
         	height: $("#layui-layer1").height(),
         	width: $("#layui-layer1").width(),
     	});
-	var pie_chart = echarts.init(document.getElementById('chart'));
-	if(pie_option){
+	var first=avg(firstTime);
+    var second=avg(secondTime);
+    var third=avg(thirdTime);
+    var fourth=avg(fourthTime);
+    var fifth=avg(fifthTime);
+    var sixth=avg(sixthTime);
+    var pie_chart = echarts.init(document.getElementById('chart'));
+    pie_option = {
+        title: {
+            text: '你的时间比例~',
+            //subtext: '纯属虚构',
+            x: 'center',
+            y: 'top'
+        },
+        tooltip:{
+        	show:true,
+        	formatter: '{b} : {c}小时',
+        	textStyle: {
+	            fontFamily: '微软雅黑',
+	            fontWeight: 'bold',
+	            fontSize: '12'
+	        }
+        },
+        series: [
+            {
+                //name: '数据',
+                name: '',
+                type: 'pie',
+                radius: ['20%', '50%'],
+                data: [
+                    {name: firstText, value: first},
+	                {name: secondText, value: second},
+	                {name: thirdText, value: third},
+	                {name: fourthText, value: fourth},
+	                {name: fifthText, value: fifth},
+	                {name: sixthText, value: sixth}
+	            ],
+	            label:{
+	                normal:{
+	                	show: true,
+	                    formatter: '{b} : {d}%',
+	                    textStyle: {
+	                        fontFamily: '微软雅黑',
+	                        fontWeight: 'bold',
+	                        fontSize: '12'
+	                    }
+	                }
+	            },
+	        }
+	    ]
+	 };
+	if(pie_chart!=0){
+		pie_chart.clear();
 		pie_chart.setOption(pie_option);
 	}else{
-		var read=avg(readTime);
-    	var code=avg(codeTime);
-    	var eat=avg(eatTime);
-    	var game=avg(gameTime);
-    	var sport=avg(sportTime);
-    	var other=avg(otherTime);
-    	pie_option = {
-        	title: {
-            	text: '你的时间比例~',
-            	//subtext: '纯属虚构',
-            	x: 'center',
-            	y: 'top'
-        	},
-        	legend: {
-            	orient: 'vertical',
-            	x: 'left',
-            	data: ['看书', '撸代码', '吃饭', '打游戏', '运动', '其它']
-        	},
-        	calculable: true,
-        	series: [
-            	{
-                	//name: '数据',
-                	name: '',
-                	type: 'pie',
-                	radius: ['20%', '50%'],
-                	data: [
-                    	{name: '看书', value: read},
-	                    {name: '撸代码', value: code},
-	                    {name: '吃饭', value: eat},
-	                    {name: '打游戏', value: game},
-	                    {name: '运动', value: sport},
-	                    {name: '其它', value: other}
-	                ],
-	                label:{
-	                	normal:{
-	                		show: true,
-	                        formatter: '{b} : {c}小时',
-	                        textStyle: {
-	                            fontFamily: '微软雅黑',
-	                            fontWeight: 'bold',
-	                            fontSize: '12'
-	                        }
-	                	}
-	                },
-	            }
-	        ]
-	    };
+		
 	    pie_chart.setOption(pie_option);
+	
 	}
+	
 }
 
